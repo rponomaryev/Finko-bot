a8a4ec3360600fb88bd674e84097f68c264e6dcd
rponomaryev <rponomaryev@gmail.com>
2026-09-18 14:02:25 +0500

    feat: add review destination for two-stage publishing

--- publisher/app/config.py
+++ publisher/app/config.py
@@ -9,6 +9,7 @@ class Settings:
     session_string: str
     publish_key: str
     destination: str = "@finkouz"
+    review_destination: int = -5547276399
 
     @classmethod
     def from_env(cls) -> "Settings":
@@ -17,4 +18,8 @@ class Settings:
             api_hash=os.environ["TELEGRAM_API_HASH"],
             session_string=os.environ["TELEGRAM_SESSION_STRING"],
             publish_key=os.environ["FINKO_PUBLISH_KEY"],
+            destination=os.environ.get("TELEGRAM_CHANNEL_DESTINATION", "@finkouz"),
+            review_destination=int(
+                os.environ.get("TELEGRAM_REVIEW_CHAT_ID", "-5547276399")
+            ),
         )
